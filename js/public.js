91d86fc8aeeba4a8dc4f97cedfa92df4eb2e9cd0
ingfrank12 <franciscohdeez@gmail.com>
2023-11-06 22:33:00 -0600

    main

---
+++ js/public.js
@@ -0,0 +1,15 @@
+document.getElementById('login-form').addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    if(username === "user" && password === "password") {
+      alert("Login successful!");
+      // Aquí podrías redirigir al usuario a su página de perfil o dashboard
+      // window.location.href = "profile.html";
+    } else {
+      document.getElementById('error-message').textContent = "Invalid username or password!";
+    }
+  });
+  
